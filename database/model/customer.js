b72efc508f5f54658bb9aeca35afa30dbd8cd1ad
Nguyen Hoang Phuc Khang <96360162+kobayashi276@users.noreply.github.com>
2023-12-15 20:09:26 +0700

    Create Customer and Order table and init the relationship each other

---
+++ database/model/customer.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Customer = sequelize.define('Customer', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+    return Customer;
+};
---
+++ database/model/order.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Order = sequelize.define('Order', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        seller: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        customerphone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+    return Order;
+};
