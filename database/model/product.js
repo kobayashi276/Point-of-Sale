266f0188f61b5139e0b7eee0ee09a652b8718502
Nguyen Hoang Phuc Khang <96360162+kobayashi276@users.noreply.github.com>
2023-12-15 18:38:15 +0700

    create product table and fix admin.ejs

---
+++ database/model/product.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Product = sequelize.define('Product', {
+    barcode: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    importprice: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    retailprice: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
+  });
+  return Product;
+};
